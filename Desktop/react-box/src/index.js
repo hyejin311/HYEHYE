544339ddfed20784212f409049de9f69709f4716
hyejin311 <go136go@naver.com>
2022-04-21 19:56:44 +0900

    react-box1

--- Desktop/react-box/src/index.js
+++ Desktop/react-box/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { FaAngellist } from "react-icons/fa";
 import './index.css';
 
 function App(){
   return(
     <article>
+      <h3>
+      <FaAngellist style={{color:'pink'}}/>
+      </h3>
       welcome
     </article>
   )
